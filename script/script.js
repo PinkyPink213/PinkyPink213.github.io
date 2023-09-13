778c5bad6c87e2ce3a0ccc72c913b4b74c98a5de
Pink <isariya@rs.co.th>
2023-09-14 00:10:02 +0800

    update ScrollToTop feature

--- script/script.js
+++ script/script.js
@@ -9,3 +9,12 @@ function scrollToTop() {
   });
 }
 scrollToTopBtn.addEventListener("click", scrollToTop);
+
+
+scrollToTopBtn.style.display = "none";//by default should be hidden
+document.querySelector('body').onscroll = function(){//whenever they scroll
+  if (window.scrollY > 150)//if scroll is 150px from top
+    scrollToTopBtn.style.display = "block";//if they scroll down, show
+  else
+  scrollToTopBtn.style.display = "none";//if they scroll up, hide
+};
